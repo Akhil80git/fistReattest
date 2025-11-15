29bd64a3134a86eb332c0c545c5e717b04275682
akhilesh <akhilgarg8085@gmail.com>
2025-11-15 08:18:07 +0530

    Update Nav.jsx

--- src/co/Nav.jsx
+++ src/co/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Nav.css";
 
-function Nav() {
+function Nav({ installApp }) {
   return (
     <nav className="topnav">
       <div className="nav-location">
@@ -10,13 +10,11 @@ function Nav() {
       </div>
       <div className="nav-search">
         <span className="nav-search-icon">&#128269;</span>
-        <input
-          className="nav-search-input"
-          type="text"
-          placeholder="Search for items..."
-        />
+        <input className="nav-search-input" type="text" placeholder="Search for items..." />
       </div>
-      <button className="nav-install-btn">Install</button>
+      <button className="nav-install-btn" onClick={installApp}>
+        Install
+      </button>
     </nav>
   );
 }
